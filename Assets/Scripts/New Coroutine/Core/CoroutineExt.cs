46bf8bd8a29228636d981fa21dbed237c340aa03
Idleman <wnt2009@gmail.com>
2014-12-16 12:38:52 +0800

    Add CoroutineExt (Some extend function)

---
+++ Assets/Scripts/New Coroutine/Core/CoroutineExt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoroutineExt {
+	public static IEnumerator WaitForAnimatorFinish(Animator animator, int layer) {
+		while( true ) {
+			AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(layer);
+
+			if( stateInfo.normalizedTime >= 1 )
+				break;
+
+			yield return null;
+		}
+	}
+}
