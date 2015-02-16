0d3312e4adc9ddaf7f0feed1247dd4e54535676c
Idleman <wnt2009@gmail.com>
2015-02-16 18:35:29 +0800

    Fix WaitForAnimatorFinish (CoroutineExt) is incorrect when animator is in transition.

--- Assets/Scripts/New Coroutine/Core/CoroutineExt.cs
+++ Assets/Scripts/New Coroutine/Core/CoroutineExt.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class CoroutineExt {
 	public static IEnumerator WaitForAnimatorFinish(Animator animator, int layer) {
 		while( true ) {
+			yield return null;
+
+			if( animator.IsInTransition(layer) )
+				continue;
+
 			AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(layer);
+
+			Debug.Log("state info name is attack ? " + stateInfo.IsName("Attack"));
+			Debug.Log("state info name is attack done ? " + stateInfo.IsName("Attack Done"));
+			Debug.Log("state info name is idle ? " + stateInfo.IsName("Idle"));
+
+			Debug.Log("animate state info = " + stateInfo.normalizedTime);
 
 			if( stateInfo.normalizedTime >= 1 )
 				break;
-
-			yield return null;
 		}
 	}
 }
