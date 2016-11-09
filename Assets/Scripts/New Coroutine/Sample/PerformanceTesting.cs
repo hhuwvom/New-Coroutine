5e8033ed0d2bd52bcb47f0ad039cad8818ed7fbd
Idleman <wnt2009@gmail.com>
2016-11-09 17:46:24 +0800

    Add Performance Testing

---
+++ Assets/Scripts/New Coroutine/Sample/PerformanceTesting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerformanceTesting : MonoBehaviour {
+	private void Start() {
+		int count = 10000;
+
+//		for( int i = 0; i < count; ++i )
+//			StartCoroutine(Flip());
+
+		for( int i = 0; i < count; ++i )
+			CoroutineExecutor.Do(Flip());
+	}
+
+	IEnumerator Flip() {
+		bool flag = false;
+		int number = 0;
+
+		while( number >= 0 ) {
+			flag = !flag;
+			++number;
+
+			yield return null;
+		}
+	}
+}
